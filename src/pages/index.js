37a4afc6ab68580b6035b6abb01e91f84eb7fb71
shockwavemonkey <shockwavemonkey+github@gmail.com>
2020-08-11 15:23:17 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,9 @@ import { NotionRenderer } from "react-notion";
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 
-export async function getStaticProps() {
-    const data = await fetch(
-        "https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b"
-    ).then(res => res.json());
-
-    return {
-        props: {
-            blockMap: data
-        }
-    };
-}
-
-export default ({ blockMap }) => (
+const data =  fetch("https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b").then(res => res.json());
+const blockMap = data.recordMap.block;
+export default () => (
     <div style={{ maxWidth: 768 }}>
         <NotionRenderer blockMap={blockMap} />
     </div>
