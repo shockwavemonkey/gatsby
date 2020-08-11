ffa23d351232f8c267ee2c813e6792c7da09ee33
shockwavemonkey <shockwavemonkey+github@gmail.com>
2020-08-11 14:47:36 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,30 @@
-import React from "react";
-import { NotionRenderer, BlockMapType } from "react-notion";
-import fetch from "node-fetch";
+import React, { Component } from 'react';
+import { NotionRenderer } from "react-notion";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
 
-export async function getStaticProps() {
-  const data: BlockMapType = await fetch(
-    "https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b"
-  ).then(res => res.json());
+class App extends Component {
+    // initially data is empty in state
+    state = { data: [] };
 
-  return {
-    props: {
-      blockMap: data
+    componentDidMount() {
+        // when component mounted, start a GET request
+        // to specified URL
+        fetch("https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b")
+            // when we get a response map the body to json
+            .then(response => response.json())
+            // and update the state data to said json
+            .then(data => this.setState({ data }));
     }
-  };
+
+
+    render() {
+        return (
+            <div style={{ maxWidth: 768 }}>
+                <NotionRenderer blockMap={this.state.data} />
+            </div>
+        );
+    }
 }
 
-const Home = ({ blockMap }) => (
-  <div
-    style={{
-      maxWidth: 708,
-      margin: "0 auto",
-      padding: "0 8px",
-      fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`
-    }}
-  >
-    <h1>
-      <title>react-notion example</title>
-    </h1>
-    <NotionRenderer blockMap={blockMap} />
-  </div>
-);
-
-export default Home;
+export default App;
