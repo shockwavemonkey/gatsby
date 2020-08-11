a1ed524c34933b29e9f67f23b44d4dfb6aa6589d
shockwavemonkey <shockwavemonkey+github@gmail.com>
2020-08-11 14:58:45 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NotionRenderer } from "react-notion";
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 
-class App extends Component {
-    // initially data is empty in state
-    state = { data: [] };
+export async function getStaticProps() {
+    const data = await fetch(
+        "https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b"
+    ).then(res => res.json());
 
-    componentDidMount() {
-        // when component mounted, start a GET request
-        // to specified URL
-        fetch("https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b")
-            // when we get a response map the body to json
-            .then(response => response.json())
-            // and update the state data to said json
-            .then(data => this.setState({ data }));
-    }
-
-
-    render() {
-        return (
-            <div style={{ maxWidth: 768 }}>
-                <NotionRenderer blockMap={this.state.data} />
-            </div>
-        );
-    }
+    return {
+        props: {
+            blockMap: data
+        }
+    };
 }
 
-export default App;
+export default ({ blockMap }) => (
+    <div style={{ maxWidth: 768 }}>
+        <NotionRenderer blockMap={blockMap} />
+    </div>
+);
