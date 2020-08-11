ed97e124901e3b1bb564219a007a6c9c2401ebb3
shockwavemonkey <shockwavemonkey+github@gmail.com>
2020-08-11 14:24:52 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
+import React from "react";
 import { NotionRenderer, BlockMapType } from "react-notion";
 import fetch from "node-fetch";
-import "react-notion/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
-import React from "react"
 
 export async function getStaticProps() {
-  const data = await fetch(
+  const data: BlockMapType = await fetch(
     "https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b"
   ).then(res => res.json());
 
@@ -16,10 +14,20 @@ export async function getStaticProps() {
   };
 }
 
-export default function Test({ blockMap }) {
-return (
-  <div style={{ maxWidth: 768 }}>
+const Home = ({ blockMap }) => (
+  <div
+    style={{
+      maxWidth: 708,
+      margin: "0 auto",
+      padding: "0 8px",
+      fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`
+    }}
+  >
+    <h1>
+      <title>react-notion example</title>
+    </h1>
     <NotionRenderer blockMap={blockMap} />
   </div>
-)
-}
+);
+
+export default Home;
