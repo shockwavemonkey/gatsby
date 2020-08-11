f39024e1ac637622ba5db8170c0be440c063b164
shockwavemonkey <shockwavemonkey+github@gmail.com>
2020-08-11 13:55:10 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import { NotionRenderer, BlockMapType } from "react-notion";
+import Head from "next/head";
+import fetch from "node-fetch";
 
-function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+export async function getStaticProps() {
+  const data: BlockMapType = await fetch(
+    "https://notion-api.precisionmining.workers.dev/v1/page/Path-Annotation-Script-9378e8e911db48f383bd42fee67acd0b"
+  ).then(res => res.json());
+
+  return {
+    props: {
+      blockMap: data
     }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
-      </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/blob/master/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+  };
 }
 
-export default Index;
+const Home = ({ blockMap }) => (
+  <div
+    style={{
+      maxWidth: 708,
+      margin: "0 auto",
+      padding: "0 8px",
+      fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"`
+    }}
+  >
+    <Head>
+      <title>react-notion example</title>
+    </Head>
+    <NotionRenderer blockMap={blockMap} />
+  </div>
+);
+
+export default Home;
